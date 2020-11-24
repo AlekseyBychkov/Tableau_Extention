2d83b14ebc300fd085334c1d7ab71b87c12add63
AlekseyBychkov <74955654+AlekseyBychkov@users.noreply.github.com>
2020-11-24 13:11:51 +0300

    Update DisplayImage.js

--- DisplayImage.js
+++ DisplayImage.js
@@ -2,22 +2,33 @@
 
 // Wrap everything in an anonymous function to avoid poluting the global namespace
 (function () {
+  const defaultIntervalInMin = '5';
+  let savedInfo;
+  let unregisterHandlerFunctions = [];
   // Use the jQuery document ready signal to know when everything has been initialized
   $(document).ready(function () {
     // Tell Tableau we'd like to initialize our extension
-    tableau.extensions.initializeAsync().then(function () {
-      // Once the extensions is initialized, ask the user to choose a sheet
-      const savedSheetName = tableau.extensions.settings.get('sheet');
-      const savedFieldName = tableau.extensions.settings.get('field');
-      if (savedSheetName && savedFieldName) {
-        returnURL(savedSheetName,savedFieldName);
-      }
-      else {
-        showChooseSheetDialog();
+    tableau.extensions.initializeAsync({'configure': configure}).then(function() {     
+      // This event allows for the parent extension and popup extension to keep their
+      // settings in sync.  This event will be triggered any time a setting is
+      // changed for this extension, in the parent or popup (i.e. when settings.saveAsync is called).
+      let currentSettings = tableau.extensions.settings.getAll();
+      fetchFilter();
+      fetchCurrentSettings();
+      if (typeof currentSettings.sheet !== "undefined") {
+        $('#inactive').hide();
+        
+        //updateExtensionBasedOnSettings(currentSettings.newSettings);
+        parseInfo(currentSettings); 
+        
+               
       }
       
-      initializeButtons ();
-      //showImage();
+      //console.log(savedSettingsInfo);
+      //console.log(settingsEvent);
+      
+
+      
     });
   });
 
@@ -27,123 +38,25 @@
 
    let unregisterEventHandlerFunction;
 
-   function showChooseSheetDialog(){
-    $('#choose_sheet_buttons').empty();
-    const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
+    function configure() { 
+      const popupUrl = `${window.location.origin}/DisplayImagesInTable/extensionDialog.html`;
     
-    const textFormat2 = $('<h4><font color="white">Select your sheet with the URL of the image</font></h4>');
-    $('#choose_sheet_buttons').append(textFormat2);
-    const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+      tableau.extensions.ui.displayDialogAsync(popupUrl, defaultIntervalInMin, { height: 500, width: 500 }).then((closePayload) => {
+        $('#inactive').hide();
+        $('#active').show();
 
-    worksheets.forEach(function (worksheet) {
-      // Declare our new button which contains the sheet name
-      const button = createButton(worksheet.name);
-
-      // Create an event handler for when this button is clicked
-      button.click(function () {
-        // Get the worksheet name which was selected
-        const worksheetName = worksheet.name;
-        tableau.extensions.settings.set('sheet', worksheetName);
-        // Close the dialog and show the data table for this worksheet
-        tableau.extensions.settings.saveAsync().then(function () {
-          $('#choose_sheet_dialog').modal('toggle');
-          showChooseSelectionDialog(worksheetName);
-        });
+        // The close payload is returned from the popup extension via the closeDialog method.
         
-        
-      });
 
-      // Add our button to the list of worksheets to choose from
-      $('#choose_sheet_buttons').append(button);
-    });
-
-    // Show the dialog
-    $('#choose_sheet_dialog').modal('toggle');
-
-   }
-
-   function showChooseSelectionDialog(worksheetName){
-    const worksheet = getSelectedSheet(worksheetName);
-
-    const text = "Select the URL of the image to display";
-    const textFormat = $('<h4><font color="white">Select the field that indicated the URL of the image to display</font></h4>');
-    
-    $('#choose_image_buttons').append(textFormat);
-    //$('#choose_image_dialog').text(worksheet.name);
-    worksheet.getSummaryDataAsync().then(function(data) {
-      const columnsTable = data.columns;
+    }).catch((error) => {
+      //  ... 
+      // ... code for error handling
       
-      columnsTable.forEach(function (name) {
-        const button2 = createButton(name.fieldName);
-        button2.click(function () {
-          const fieldName = name.fieldName;
-          tableau.extensions.settings.set('field', fieldName);
-          tableau.extensions.settings.saveAsync().then(function () {
-            $('#choose_image_dialog').modal('toggle');
-            returnURL(worksheetName,fieldName);
-            });
-        });
-
-        $('#choose_image_buttons').append(button2);
-      });
     });
-    
-    $('#choose_image_dialog').modal('toggle');
-    //returnURL(worksheetName);
-   }
-
-
-  
-
-  function returnURL(worksheetName, fieldName){
-    if (unregisterEventHandlerFunction) {
-      unregisterEventHandlerFunction();
-    }
-    const worksheet = getSelectedSheet(worksheetName);
-    
-    // Set our title to an appropriate value
-    worksheet.getSelectedMarksAsync().then(function (marks) {
-      // Get the first DataTable for our selected marks (usually there is just one)
-      const worksheetData = marks.data[0];
-      var index, indexName = 0;
-      for(var i =0; i<worksheetData.columns.length;i++){
-
-          if (worksheetData.columns[i].fieldName==fieldName) {
-              index = worksheetData.columns[i].index;
-          }
-          else if (worksheetData.columns[i].fieldName=="Product Name") {
-              indexName = worksheetData.columns[i].index;
-          }
-      }
-
-      // Map our data into the format which the data table component expects it
-      const data = worksheetData.data.map(function (row) {
-         const rowData = row.map(function (cell) {
-          return cell.formattedValue;
-        });
-
-        return ([rowData[index], rowData[indexName]] );
-            
-
-        ;
-      });
-
-      
-
-      
-
-      // Populate the data table with the rows and columns we just pulled out
-      displayURL(data);
-
-
-    });
-    
-    unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, function (selectionEvent) {
-      
-      returnURL(worksheetName,fieldName);
-    });
-
   }
+
+
+
 
   function getSelectedSheet (worksheetName) {
     // go through all the worksheets in the dashboard and find the one we want
@@ -153,29 +66,53 @@
   }
 
 
-  function displayURL(URL){
+  function displayImages(images,columnsData,columnsName){
     $('#selected_marks').empty();
-    console.log(URL);
-    for (var i = 0; i < URL.length; i++) {
-     var str = URL[i][0]+" ";
-     var tableImages = str.split(",");
-     const image = $(`<img style="margin: 20px 20px" src="
-      ${tableImages}"width="25%" height="25%">`);
+    const header = $(`<th><center>Images</center></th>`);
+    $('#selected_marks').append(header);
+    var tableHeader;
+    for (var i = 0; i < columnsName.length; i++) {
+      
+      tableHeader = $(`<th><center>${columnsName[i]}</center></th>`);
+      $('#selected_marks').append(tableHeader);
+    }
+    
+    for (var i = 0; i < images.length; i++) {
 
-     $('#selected_marks').append(image);
+      var str = images[i][0]+" ";
+      var tableImages = str.split(",");
+      const start = $(`<tr>`);
+      $('#selected_marks').append(start);
+
+      const image = $(`<td><center><img src="
+      ${tableImages}"width="150" height="150"></center></td>`);
+      $('#selected_marks').append(image);
+      for (var j = 0; j < columnsName.length; j++) {
+
+          const test = $(`<td><center>${columnsData[i*columnsName.length+j]}</center></td>`);
+          $('#selected_marks').append(test);
+        
+        
+        
+      }
+      
+
    }
 
+
+
+    
+    
+    
    
-     
   }
 
-  function createButton (buttonTitle) {
-    const button =
-    $(`<button type='button' class='btn btn-default btn-block'>
-      ${buttonTitle}
-    </button>`);
-    return button;
+   function updateExtensionBasedOnSettings(settings) {
+    if (settings) {
+      savedInfo = settings;
+    }
   }
+
 
   // This variable will save off the function we can call to unregister listening to marks-selected events
 
@@ -192,4 +129,132 @@
       return sheet.name === worksheetName;
     });
   }
+
+  function parseInfo(settings){
+    if (unregisterEventHandlerFunction) {
+      unregisterEventHandlerFunction();
+    }
+    
+    var worksheetsName = settings.sheet;
+    const worksheet = getSelectedSheet(worksheetsName);
+    unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.FilterChanged, function (selectionEvent) {
+     
+      parseInfo(settings);
+    });
+    var indexImage = settings.selectedImage[1];
+    var cleanIndex = settings.selectedColumns.slice(1, settings.selectedColumns.length - 1);
+    var indexColumnstable = cleanIndex.split(",");
+    var columnsName = [];
+    var columnsData = [];
+    worksheet.getSummaryDataAsync().then(function (marks) {      
+      const worksheetData = marks;
+      
+      for (var i = 0; i < indexColumnstable.length; i++) {
+        var index = indexColumnstable[i];
+        //console.log(worksheetData.columns[index]);
+        columnsName.push(worksheetData.columns[index].fieldName);
+      }
+
+       const data = worksheetData.data.map(function (row) {
+         const rowData = row.map(function (cell) {
+          
+          return cell.formattedValue;
+          });
+        for (var i = 0; i < indexColumnstable.length; i++) {
+          var index2 = indexColumnstable[i];
+          columnsData.push(rowData[index2]);
+      }
+      });
+      
+      
+      
+      const image = worksheetData.data.map(function (row) {
+         const rowData = row.map(function (cell) {
+          
+
+          return cell.formattedValue;
+
+          });
+      return ([rowData[indexImage]]);
+    });
+      // Populate the data table with the rows and columns we just pulled out
+      
+        displayImages(image,columnsData,columnsName);
+    });
+  }
+
+  function isOdd(num) { return num % 2;}
+
+  function fetchCurrentSettings() {
+        // While performing async task, show loading message to user.
+        //$('#loading').addClass('show');
+
+        // Whenever we restore the filters table, remove all save handling functions,
+        // since we add them back later in this function.
+        unregisterHandlerFunctions.forEach(function(unregisterHandlerFunction) {
+            unregisterHandlerFunction();
+        });
+
+        // Since filter info is attached to the worksheet, we will perform
+        // one async call per worksheet to get every filter used in this
+        // dashboard.  This demonstrates the use of Promise.all to combine
+        // promises together and wait for each of them to resolve.
+        let filterFetchPromises = [];
+
+        // List of all filters in a dashboard.
+        let dashboardfilters = [];
+
+        // To get filter info, first get the dashboard.
+        const dashboard = tableau.extensions.dashboardContent.dashboard;
+
+        tableau.extensions.settings.addEventListener(tableau.TableauEventType.SettingsChanged, (settingsEvent) => {
+            //console.log(settingsEvent);
+            //updateExtensionBasedOnSettings(settingsEvent.newSettings);
+            parseInfo(settingsEvent.newSettings);
+        });
+    }
+
+    function fetchFilter() {
+        // While performing async task, show loading message to user.
+        //$('#loading').addClass('show');
+
+        // Whenever we restore the filters table, remove all save handling functions,
+        // since we add them back later in this function.
+        unregisterHandlerFunctions.forEach(function(unregisterHandlerFunction) {
+            unregisterHandlerFunction();
+        });
+
+        // Since filter info is attached to the worksheet, we will perform
+        // one async call per worksheet to get every filter used in this
+        // dashboard.  This demonstrates the use of Promise.all to combine
+        // promises together and wait for each of them to resolve.
+        let filterFetchPromises = [];
+
+        // List of all filters in a dashboard.
+        let dashboardfilters = [];
+
+        // To get filter info, first get the dashboard.
+        const dashboard = tableau.extensions.dashboardContent.dashboard;
+
+        // Then loop through each worksheet and get its filters, save promise for later.
+        dashboard.worksheets.forEach(function(worksheet) {
+            //filterFetchPromises.push(worksheet.getFiltersAsync());
+
+            // Add filter event to each worksheet.  AddEventListener returns a function that will
+            // remove the event listener when called.
+            let unregisterHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.FilterChanged, filterChangedHandler);
+            //unregisterHandlerFunctions.push(unregisterHandlerFunction);
+        });
+    }
+
+  function filterChangedHandler(filterEvent) {
+        // Just reconstruct the filters table whenever a filter changes.
+        // This could be optimized to add/remove only the different filters.
+        //fetchFilters();
+        //reload gauge
+        const settingsSaved = tableau.extensions.settings.getAll();
+        parseInfo(settingsSaved);
+    }
+
+
 })();
