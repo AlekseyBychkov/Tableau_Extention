0fc2b97effc461c82f340be5a320a2d4473a7983
AlekseyBychkov <74955654+AlekseyBychkov@users.noreply.github.com>
2020-11-24 15:09:49 +0300

    Update DisplayImage.js

--- DisplayImage.js
+++ DisplayImage.js
@@ -87,14 +87,11 @@
       const image = $(`<td><center><img src="
       ${tableImages}"width="100" height="100"></center></td>`);
       $('#selected_marks').append(image);
-      for (var j = 0; j < columnsName.length; j++) {
-
+      
+      /*for (var j = 0; j < columnsName.length; j++) {
           const test = $(`<td><center>${columnsData[i*columnsName.length+j]}</center></td>`);
           $('#selected_marks').append(test);
-        
-        
-        
-      }
+      }*/
       
 
    }
